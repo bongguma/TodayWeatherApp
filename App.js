b49d53a939abdaf42cfd73e5af10332232b175de
kimyejin <7602kyj@e-mirim.hs.kr>
2020-12-15 23:58:20 +0900

    현재 자신의 위치 location 을 받아오는 기능 구현 작업 (권한 설정 기능 구현이 필요)

--- App.js
+++ App.js
@@ -3,21 +3,39 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Loading from './Screen/Loadng';
 
-export default function App() {
-  return (
-    <Loading />
-    // expo-cli를 이용해 기본 스타일 구현 작업
-    // <View style={styles.container}>
-    //   <View style={styles.yellowView}>
-    //     <Text Style={styles.text}>Hello! yellow</Text>
-    //   </View>
-    //   <View style={styles.greenView}>
-    //     <Text Style={styles.text}>Hello! green</Text>
-    //   </View>
-    //   <StatusBar styles="auto" />
-    // </View>
-  );
+import * as Loaction from 'expo-location';
+
+export default class extends React.Component {
+  getLocation = async () => {
+    const location = await Loaction.getCurrentPositionAsync();
+    console.log(location);
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render(){
+    return <Loading />;
+  }
 }
+
+// function 함수형 컴포넌트식으로 리액트 네이티브 구현 방식
+// export default function App() {
+//   return (
+//     <Loading />
+//     // expo-cli를 이용해 기본 스타일 구현 작업
+//     // <View style={styles.container}>
+//     //   <View style={styles.yellowView}>
+//     //     <Text Style={styles.text}>Hello! yellow</Text>
+//     //   </View>
+//     //   <View style={styles.greenView}>
+//     //     <Text Style={styles.text}>Hello! green</Text>
+//     //   </View>
+//     //   <StatusBar styles="auto" />
+//     // </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
