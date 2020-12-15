61b72ad18fd2c81fb62e7a13f120410005a6ff7a
kimyejin <7602kyj@e-mirim.hs.kr>
2020-12-15 23:12:32 +0900

    style를 이용해 기본적인 expo-cli 기능 구현

--- App.js
+++ App.js
@@ -5,8 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.yellowView}>
+        <Text Style={styles.text}>Hello! yellow</Text>
+      </View>
+      <View style={styles.greenView}>
+        <Text Style={styles.text}>Hello! green</Text>
+      </View>
+      <StatusBar styles="auto" />
     </View>
   );
 }
@@ -17,5 +22,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  yellowView: {
+    flex: 1,
+    backgroundColor:'yellow'
+  },
+  greenView: {
+    flex: 1,
+    backgroundColor:'green'
+  },
+  text: {
+    color:'black',
   },
 });
